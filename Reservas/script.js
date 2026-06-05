1cfadfe7d8379234acf43e19ad146439d6e4280a
Chala785 <juan_gallego82241@elpoli.edu.co>
2026-06-05 17:51:46 -0500

    Nuevos cambios realizados

--- Reservas/script.js
+++ Reservas/script.js
@@ -1,4 +1,22 @@
-  function selectCard(el, gridId) {
+//menu hamburguesa
+const ham = document.getElementById('ham');
+    const menu = document.getElementById('mobileMenu');
+
+    ham.addEventListener('click', () => {
+      ham.classList.toggle('open');
+      menu.classList.toggle('open');
+    });
+
+    // Cerrar menú al hacer clic en un enlace
+    menu.querySelectorAll('a').forEach(link => {
+      link.addEventListener('click', () => {
+        ham.classList.remove('open');
+        menu.classList.remove('open');
+      });
+    });
+
+// Lógica de selección de alojamiento y addons
+function selectCard(el, gridId) {
     document.querySelectorAll(`#${gridId} .card`).forEach(c => c.classList.remove('selected'));
     el.classList.add('selected');
     updateButton();
